babe2b395a2ee0d7abcd5656e87fbfc7ffda4459
galle2018 <hfsoluciones@hotmail.es>
2021-11-19 08:44:05 -0300

    Estrutura basica inicial com dependencias do H2 para implementacao inicial

---
+++ src/main/java/br/com/barter/APIbarter/ApiBarterApplication.java
@@ -0,0 +1,13 @@
+package br.com.barter.APIbarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/barter/APIbarter/ApiBarterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barter.APIbarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
