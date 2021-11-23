90c9007a374a0de607769d31b2a3e6c08b693322
Luiza Duarte <duartes.luiza92@gmail.com>
2021-11-23 11:47:57 -0300

    Criada a documentação SWAGGER da API. Foi criada a classe SwaggerConfig no pacote config e  foram adicionados os parâmetros do swagger nas classes controllers

--- src/main/java/br/com/barter/APIbarter/ApiBarterApplication.java
+++ src/main/java/br/com/barter/APIbarter/ApiBarterApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+
 public class ApiBarterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/barter/APIbarter/controllers/CategoriaRestController.java
+++ src/main/java/br/com/barter/APIbarter/controllers/CategoriaRestController.java
@@ -17,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.barter.APIbarter.dto.CategoriaDto;
 import br.com.barter.APIbarter.modelos.Categoria;
 import br.com.barter.APIbarter.service.api.CategoriaServiceAPI;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/categorias/api/v1/")
-@CrossOrigin("*")
+@Api(value = "API REST Api-barter")
+@CrossOrigin(origins="*")
 public class CategoriaRestController {
 	
 	@Autowired
@@ -28,17 +31,20 @@ public class CategoriaRestController {
 
 	@GetMapping(value = "/all")
 	@Cacheable(value="listaDeCategorias")
+	@ApiOperation(value="Retorna uma lista de Categorias")
 	public List<CategoriaDto> getAll() throws Exception {
 		return categoriaServiceAPI.getAll();
 		
 	}
 
 	@GetMapping(value = "/find/{id}")
+	@ApiOperation(value="Retorna categoria única por id")
 	public CategoriaDto find(@PathVariable String id) throws Exception {
 		return categoriaServiceAPI.get(id);
 	}
 	
 	@PostMapping(value = "/save/{id}")
+	@ApiOperation(value="Salva uma categoria")
 	public ResponseEntity<String> save(@RequestBody Categoria categoria, @PathVariable String id) throws Exception {
 	
 		if (id == null || id.length() == 0 || id.equals("null")) {
@@ -52,6 +58,7 @@ public class CategoriaRestController {
 
 	
 	@GetMapping(value = "/delete/{id}")
+	@ApiOperation(value="Deleta uma categoria por id")
 	public ResponseEntity<CategoriaDto> delete(@PathVariable String id) throws Exception {
 		CategoriaDto categoria = categoriaServiceAPI.get(id);
 		
